074bdac1595aea2ea7de8d48bc68fbe63bfb14fa
elshacgr <s21910278@student.unklab.ac.id>
2022-04-22 17:15:37 +0800

    Project App Added

---
+++ ios/Pods/Target Support Files/Pods-UMate-UMateTests/Pods-UMate-UMateTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UMate_UMateTests : NSObject
+@end
+@implementation PodsDummy_Pods_UMate_UMateTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-UMate-UMateTests/Pods-UMate-UMateTests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_UMate_UMateTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_UMate_UMateTestsVersionString[];
+
