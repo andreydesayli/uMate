35df7bcbbe8653c83cea3415f947e93549c70896
Itou-Liando <93631488+Itou-Liando@users.noreply.github.com>
2022-05-02 18:54:48 +0800

    tambah halaman reset password

---
+++ src/pages/ResetPass/index.js
@@ -0,0 +1,59 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { IconBack } from '../../assets/icons'
+import { Button, TextInput, Gap } from '../../components'
+
+const ResetPass = ({navigation, title='Reset Password', onBack}) => {
+  return (
+    <View>
+        <View style={styles.headerWrapper}>
+        {onBack && (
+        <TouchableOpacity onPress={onBack} activeOpacity={0.7}>
+        <View style={styles.back}>
+            
+        </View>
+        </TouchableOpacity>
+        )}
+    <IconBack />
+    <Gap width={20} />
+    <Text style={styles.text}>{title}</Text>
+  </View>
+  <Gap height={20} />
+  <Text style={styles.text3}>New Password</Text>
+  <TextInput mL={40} mR={40}  placeholder='  *****'></TextInput>
+  <Gap height={20} />
+  <Text style={styles.text3}>Confirm Password</Text>
+  <TextInput mL={40} mR={40}  placeholder='  *****'></TextInput>
+  <Gap height={50} />
+  <Button mL={120} mR={120} style={styles.button1} title={'Save'} onPress={() => navigation.navigate('')}>Next</Button>
+  </View>
+
+  )
+}
+
+export default ResetPass
+
+const styles = StyleSheet.create({
+    headerWrapper: {
+        backgroundColor: '#D3D3D380',
+        paddingTop: 40,
+        paddingBottom: 33,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    back:{
+        padding: 10,
+    }, 
+    text: {
+        fontSize: 35, 
+        fontFamily: 'Roboto-Bold',
+        color: 'black',
+        marginLeft: 20
+    },
+    text3: {
+        fontSize: 15,
+        fontFamily: 'Roboto-Bold',
+        color: 'black',
+        marginLeft: 40
+    },
+})
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,5 +4,6 @@ import SignUp from "./SignUp";
 import RegisOne from "./RegisOne";
 import RegisTwo from "./RegisTwo";
 import RegisThree from "./RegisThree";
+import ResetPass from "./ResetPass";
 
-export {SplashScreen, SignIn, SignUp, RegisOne, RegisTwo, RegisThree};
+export {SplashScreen, SignIn, SignUp, RegisOne, RegisTwo, RegisThree, ResetPass};
